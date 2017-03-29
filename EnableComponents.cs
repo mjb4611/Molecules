c4a959fa46277994bda57ca0d7f4b897324d8352
mjb4611 <mjb4611@rit.edu>
2017-03-29 18:43:46 -0400

    Add files via upload

---
+++ EnableComponents.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableComponents : MonoBehaviour
+{
+    private Spin mySpin;
+
+
+    void Start()
+    {
+        mySpin = GetComponent<Spin>();
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                mySpin.enabled = !mySpin.enabled;
+            }
+        }
+    }
+}
